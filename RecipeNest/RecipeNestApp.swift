2538716c189644030ade3d2c3bf8cd733a70cbe0
Jatin Foujdar <jatin321001@gmail.com>
2025-04-29 12:43:24 +0530

    Initial Commit

---
+++ RecipeNest/RecipeNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeNestApp.swift
+//  RecipeNest
+//
+//  Created by jatin foujdar on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
